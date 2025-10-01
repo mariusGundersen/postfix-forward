fdfa5b7ac4750b3ad3e975719161acad8ff36e30
Marius Gundersen <me@mariusgundersen.net>
2025-10-01 22:04:34 +0200

    reenabled spam protection

--- startup.sh
+++ startup.sh
@@ -113,12 +113,12 @@ postconf -e "smtpd_helo_restrictions=\
   reject_invalid_helo_hostname,\
   permit"
 
-#postconf -e "smtpd_client_restrictions=\
-#  reject_rbl_client cbl.abuseat.org,\
-#  reject_rbl_client sbl-xbl.spamhaus.org,\
-#  reject_rbl_client bl.spamcop.net, \
-#  reject_rhsbl_sender dsn.rfc-ignorant.org,\
-#  "
+postconf -e "smtpd_client_restrictions=\
+  reject_rbl_client cbl.abuseat.org,\
+  reject_rbl_client sbl-xbl.spamhaus.org,\
+  reject_rbl_client bl.spamcop.net, \
+  reject_rhsbl_sender dsn.rfc-ignorant.org,\
+  "
 
 postconf -e "smtpd_recipient_restrictions=\
   permit_sasl_authenticated,\
